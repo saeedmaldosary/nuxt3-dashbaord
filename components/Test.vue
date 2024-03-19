099876a270a83deff48bbd56154e368ad7143108
Saeed Aldosary <saldosary@scai.sa>
2024-03-19 12:42:18 +0300

    Menu component

--- components/Test.vue
+++ components/Test.vue
@@ -1,4 +1,18 @@
 <template>
-  <div>Test component</div>
-  <v-btn color="red" variant="outlined" prepend-icon="$vuetify"> Button </v-btn>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      Main Content
+    </v-main>
+  </v-layout>
 </template>
